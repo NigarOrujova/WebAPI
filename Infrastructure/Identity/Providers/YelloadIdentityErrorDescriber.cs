fbd180047c6dcaf6d5559326b79ac1c2ba0394b2
NigarOrujova <nigar.orucova.e@student.aztu.edu.az>
2023-10-09 16:28:16 +0400

    feat: added IdentityModules

---
+++ Infrastructure/Identity/Providers/YelloadIdentityErrorDescriber.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Providers;
+
+public class YelloadIdentityErrorDescriber : IdentityErrorDescriber
+{
+    public override IdentityError DuplicateRoleName(string role)
+    {
+        //return base.DuplicateRoleName(role);
+        return new IdentityError
+        {
+            Code = nameof(DuplicateRoleName),
+            Description = $"{role} bu rol artiq movcuddur"
+        };
+    }
+
+    public override IdentityError InvalidRoleName(string role)
+    {
+        return new IdentityError
+        {
+            Code = nameof(InvalidRoleName),
+            Description = "rol adi bosh buraxila bilmez"
+        };
+    }
+
+    public override IdentityError DuplicateEmail(string email)
+    {
+        //return base.DuplicateEmail(email);
+
+        return new IdentityError
+        {
+            Code = nameof(DuplicateEmail),
+            Description = $"'{email}' artiq movcuddur"
+        };
+    }
+
+    public override IdentityError UserAlreadyInRole(string role)
+    {
+        return new IdentityError
+        {
+            Code = nameof(UserAlreadyInRole),
+            Description = $"İstifadəçi hazırda {role}-roldadır"
+        };
+    }
+
+    public override IdentityError UserNotInRole(string role)
+    {
+        return new IdentityError
+        {
+            Code = nameof(UserNotInRole),
+            Description = $"İstifadəçi {role}-rolda deyil"
+        };
+    }
+}
