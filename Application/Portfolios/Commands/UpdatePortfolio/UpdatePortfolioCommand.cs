7a245a81e48a6949494e50b5044bb486cf974171
NigarOrujova <nigar.orucova.e@student.aztu.edu.az>
2023-11-06 16:48:33 +0400

    .

--- Application/Portfolios/Commands/UpdatePortfolio/UpdatePortfolioCommand.cs
+++ Application/Portfolios/Commands/UpdatePortfolio/UpdatePortfolioCommand.cs
@@ -1,17 +1,25 @@
 ﻿using Application.Abstracts.Common.Interfaces;
+using Application.Dtos.Images;
+using Application.Extensions;
 using Domain.Entities;
 using MediatR;
+using Microsoft.Extensions.Hosting;
 
 namespace Application.Portfolios.Commands.UpdatePortfolio;
 
-public record UpdatePortfolioCommand(int Id,Portfolio Portfolio) : IRequest<Portfolio>;
+public record UpdatePortfolioCommand(int Id,Portfolio Portfolio) : IRequest<Portfolio>
+{
+    public List<ImageDto>? Images { get; set; }
+}
 public class UpdatePortfolioCommandHandler : IRequestHandler<UpdatePortfolioCommand, Portfolio>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IHostEnvironment _env;
 
-    public UpdatePortfolioCommandHandler(IUnitOfWork unitOfWork)
+    public UpdatePortfolioCommandHandler(IUnitOfWork unitOfWork, IHostEnvironment env)
     {
         _unitOfWork = unitOfWork;
+        _env = env;
     }
 
     public async Task<Portfolio> Handle(UpdatePortfolioCommand request, CancellationToken cancellationToken)
@@ -34,6 +42,26 @@ public class UpdatePortfolioCommandHandler : IRequestHandler<UpdatePortfolioComm
                     CategoryId = categoryId
                 };
                 entity.PortfolioCategories.Add(portfolioCategory);
+            }
+        }
+        entity.Images = new List<PortfolioImage>();
+        if (request.Images != null)
+        {
+            foreach (var item in request.Images)
+            {
+                if (item != null)
+                {
+                    var image = new PortfolioImage();
+
+                    image.ImagePath = item.Image.GetRandomImagePath("portfolio");
+
+                    await _env.SaveAsync(item.Image, image.ImagePath, cancellationToken);
+
+                    image.IsMain = item.IsMain;
+                    image.ImageAlt = item.ImageAlt;
+
+                    entity.Images.Add(image);
+                }
             }
         }
         await _unitOfWork.PortfolioRepository.UpdateAsync(entity);
--- Domain/Entities/PortfolioImage.cs
+++ Domain/Entities/PortfolioImage.cs
@@ -5,9 +5,9 @@ namespace Domain.Entities;
 
 public class PortfolioImage:BaseEntity
 {
-    public string ImagePath { get; set; } = null!;
-    public IFormFile Image { get; set; } 
-    public string ImageAlt { get; set; } = null!;
+    public string? ImagePath { get; set; }
+    public IFormFile? Image { get; set; } 
+    public string? ImageAlt { get; set; }
     public bool IsMain { get; set; }
     public int? PortfolioId { get; set; }
     public Portfolio? Portfolio { get; set; }
